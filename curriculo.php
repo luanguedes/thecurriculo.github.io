a5d526de474e0ac60a250101fe8ba73083529115
luanguedes <luan.guedes@edu.unipar.br>
2018-08-31 16:59:43 -0300

    Commit 1 - HTML/CSS

---
+++ curriculo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Curriculo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<center>
+		<h1 class="titulo2">THE CURRICULO</h1>
+		<h2>Preencha seus dados pessoais:</h2>
+		<br>
+		<br>
+		<p>Nome:     <input type="text" name="nome"></p>
+		<p>Data de Nascimento: <input type="date" name="nome"></p>
+		<p>Estado Civil: <select name="estado_civil">
+      		<option value="Casado (a)">Casado (a)</option>
+      		<option value="Solteiro (a)">Solteiro (a)</option>
+      		<option value="Viuvo (a)">Viuvo (a)</option>
+      		<option value="Divorcidado (a)">Divorciado (a)</option>
+    	</select></p><br><br><br>
+    	<p>Endereço: <input type="text" name="endereco"></p>
+    	<p>Nº:       <input type="text" name="numero"></p>
+    	<p>Bairro:   <input type="text" name="bairro"></p><br><br><br>
+    	<p>C.E.P:    <input type="text" name="cep"></p>
+    	<p>Cidade:   <input type="text" name="cidade"></p>
+    	<p>Estado:   <input type="text" name="estado"></p>
+	</center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Curriculo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<center>
+		<h1 class="titulo">THE CURRICULO</h1>
+		<a href="curriculo.php"><button>CRIE SEU CURRICULO GRATIS</button></a>
+	</center>
+</body>
+</html>
